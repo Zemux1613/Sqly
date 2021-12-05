730e3557c58d4cb26b7038b517fa4ee04fcbdc47
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2021-12-05 13:56:05 +0100

    feat: change method heads

--- src/main/java/de/sqly/interfaces/ISqlyConnection.java
+++ src/main/java/de/sqly/interfaces/ISqlyConnection.java
@@ -1,11 +1,13 @@
 package de.sqly.interfaces;
 
+import de.sqly.exceptions.ConnectionFailedException;
+
 import java.sql.ResultSet;
 import java.util.concurrent.CompletableFuture;
 
 public interface ISqlyConnection {
 
-    void connect();
+    void connect() throws ConnectionFailedException;
     void disconnect();
     boolean isConnected();
     void executeUpdate(String qry);
